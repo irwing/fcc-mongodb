23efa7063f32ee1a1c5e98cc98e2dbdfbd7dfdd1
Irwing Naranjo <khalisser@gmail.com>
2021-11-22 00:03:45 -0300

    Create a Model

---
+++ models/person.model.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+
+const Schema = mongoose.Schema;
+
+const PersonSchema = new Schema({
+  name: { type: String, required: true },
+  age: { type: String, required: true },
+  favoriteFoods: { type: Array },
+});
+
+module.exports = mongoose.model('Person', PersonSchema);
--- myApp.js
+++ myApp.js
@@ -1,9 +1,6 @@
 require('dotenv').config();
-const mongoose = require('mongoose');
 
-const conection = mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
-
-let Person;
+let Person = require("./models/person.model");
 
 const createAndSavePerson = (done) => {
   done(null /*, data*/);

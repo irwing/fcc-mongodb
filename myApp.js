18dd0f0d72b90aa7c23d7329bf2416f6915a4bc6
Irwing Naranjo <khalisser@gmail.com>
2021-11-25 20:36:46 -0300

    Chain Search Query Helpers to Narrow Search Results

--- myApp.js
+++ myApp.js
@@ -93,7 +93,14 @@ const removeManyPeople = (done) => {
 const queryChain = (done) => {
   const foodToSearch = "burrito";
 
-  done(null /*, data*/);
+  Person.find({ favoriteFoods: foodToSearch })
+    .sort({ name: 1 })
+    .limit(2)
+    .select({ age: 0 })
+    .exec(function(err, peoples) {
+      if(err) return console.log(err);
+      done(null, peoples);
+    });
 };
 
 /** **Well Done !!**
